7c8539f2fbaefa677205d5120bec004eb5a3b8d5
Kwame-Baffoe <khlikybhyteservices@gmail.com>
2024-08-20 13:38:43 -0500

    Updated to solve redeclaration of variable

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -17,6 +17,7 @@ for (let i=0; i<4; i++) {
 console.log('count from 0 to 5');
 for (let i=0; i<6; i++) {
   console.log(i);
+}
 
 
 
@@ -28,6 +29,7 @@ for (let i=0; i<6; i++) {
 console.log('count from 3 to 5');
 for (let i=3; i<6; i++) {
   console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
@@ -35,8 +37,9 @@ for (let i=3; i<6; i++) {
         // we need to change the condition to i % 2 === 0 and the start value to 2
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 for (let i=2; i<11; i++) {
-  if (i % 2 === 0) {
     console.log(i);
+}
+
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
@@ -44,6 +47,7 @@ console.log('STRETCH: countdown from 5 to 0');
 // we need to change the start value to 5 and the condition to i > 0
 for (let i=5; i>0; i--) {
   console.log(i);
+}
 
 
 
